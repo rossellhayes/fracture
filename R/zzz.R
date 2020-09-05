9f4c6e8cca2b89bc29ae0eb86c31f64504a92bb8
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-09-05 12:25:21 -0700

    Check sanitizers manually with Rhub rather than through GitHub Actions

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onUnload <- function(libpath) {
   library.dynam.unload("fracture", libpath)
 }
+
+release_questions <- function() {
+  "Have you checked sanitizers (with `rhub::check_with_sanitizers()`)?"
+}
